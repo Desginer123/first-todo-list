0ec67d07e0d175a1d2baba807cad6472b2b0f9ee
Desginer123 <hotsru12@gmail.com>
2021-09-18 23:39:50 +0800

    task_list Ready

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,17 @@ let form = document.getElementById('submit_task'),
 	input = document.getElementById('task_name'),
 	btn = document.getElementById('save_task');
 
-
+let startArr = JSON.parse(localStorage.getItem('key'))
 
 let task_list = []
 
+if(startArr) {
+	task_list = JSON.parse(localStorage.getItem('key'))
+}
 
 btn.addEventListener('click', function(e){
 	e.preventDefault();
-	if(input.value != '') {
+	if(input.value !== '') {
 		let inputValue = input.value;
 		let newTask = {
 			task_name: inputValue,
@@ -27,7 +30,6 @@ btn.addEventListener('click', function(e){
 
 
 		return returnObj
-
 
 	}
 
